f08093fb28741b0fde5f6a89782b0088d5d48a22
kei ikeuchi <sister@sister.local>
2016-10-07 16:18:42 +0900

    done

--- Songbird/ViewController.swift
+++ Songbird/ViewController.swift
@@ -1,23 +1,41 @@
-//
-//  ViewController.swift
-//  Songbird
-//
-//  Created by kei ikeuchi on 2016/10/07.
-//  Copyright © 2016年 mycompany. All rights reserved.
-//
+
 
 import UIKit
+import ESTabBarController
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    func setupTab() {
+        let tabBarController = ESTabBarController(tabIconNames: ["home", "camera", "setting"])
+        
+        tabBarController.selectedColor = UIColor(red: 1.0, green: 0.44, blue: 0.11, alpha: 1)
+        tabBarController.buttonsBackgroundColor = UIColor(red: 0.96, green: 0.91, blue: 0.87, alpha: 1)
+        
+        addChildViewController(tabBarController)
+        view.addSubview(tabBarController.view)
+        tabBarController.view.frame = view.bounds
+        tabBarController.didMoveToParentViewController(self)
+        
+        let homeViewController = storyboard?.instantiateViewControllerWithIdentifier("Home")
+        let settingViewController = storyboard?.instantiateViewControllerWithIdentifier("Setting")
+        
+        tabBarController.setViewController(homeViewController, atIndex: 0)
+        tabBarController.setViewController(settingViewController, atIndex: 2)
+        
+        tabBarController.highlightButtonAtIndex(1)
+        tabBarController.setAction({
+    
+            let imageViewController = self.storyboard?.instantiateViewControllerWithIdentifier("ImageSelect")
+            self.presentViewController(imageViewController!, animated: true, completion: nil)
+            }, atIndex: 1)
     }
 
 
