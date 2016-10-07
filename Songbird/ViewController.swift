aa859394cd4af65d46f9389d2ae7396180e9f116
kei ikeuchi <sister@sister.local>
2016-10-07 11:50:27 +0900

    Initial Commit

---
+++ Songbird/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Songbird
+//
+//  Created by kei ikeuchi on 2016/10/07.
+//  Copyright © 2016年 mycompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
